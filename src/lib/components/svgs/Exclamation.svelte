2e3fc90020c4e1f0207d6719b7f010b835b2e0f1
AmreetKhuntia <amreetleo123@gmail.com>
2025-07-11 23:35:28 +0530

    feat: added tooltip to home
    - added tooltip to home

---
+++ src/lib/components/svgs/Exclamation.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  export let strokeColor: string = '#000000';
+</script>
+
+<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
+  ><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g
+    id="SVGRepo_tracerCarrier"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  ></g><g id="SVGRepo_iconCarrier">
+    <path
+      d="M12.01 20H12M12 14L12.01 4"
+      stroke={strokeColor}
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    ></path>
+  </g></svg
+>
