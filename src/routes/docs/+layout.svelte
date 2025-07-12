befbaec849b18450a2f8326fdc2f9b9963ad4e7b
AmreetKhuntia <amreetleo123@gmail.com>
2025-07-12 19:58:31 +0530

    feat: added active item binding for setting active item
    - added active item binding for setting active item in navbar

--- src/routes/docs/+layout.svelte
+++ src/routes/docs/+layout.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
   import { goto } from '$app/navigation';
   import { type DropdownItem, type SideNavItem, DropDown, SideNav } from '$lib';
+  import { onMount } from 'svelte';
   export let data: { items: SideNavItem[] };
+
   let versions: DropdownItem[] = [
     {
       id: 'v1.0',
@@ -20,17 +22,33 @@
       active: false,
     },
   ];
+  let currentActiveItem: SideNavItem | undefined = undefined;
 
   function onClick(currentLayerIndex: number, item: SideNavItem): void {
     if (item.href !== '') {
       goto(item.href);
     }
   }
+
+  function findAndSetActiveItem(items: SideNavItem[], path: string) {
+    for (const item of items) {
+      if (item.children !== undefined && item.children.length > 0) {
+        findAndSetActiveItem(item.children, path);
+      } else if (item.href === path) {
+        currentActiveItem = item;
+        return;
+      }
+    }
+  }
+  onMount(() => {
+    const currentPath = window.location.pathname;
+    findAndSetActiveItem(data.items, currentPath);
+  });
 </script>
 
 <div class="docs-layout">
   <aside class="sidebar">
-    <SideNav items={data.items} {onClick}>
+    <SideNav items={data.items} {onClick} bind:activeItem={currentActiveItem}>
       <div slot="side-nav-top-content" class="sidebar-top-content">
         <div class="side-nav-top-content-header">Version</div>
         <DropDown items={versions} multi={false} buttonLabel={'Version'} />
