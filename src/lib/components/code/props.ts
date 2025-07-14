8be3a802153ea2fdc4121e431ed05a6b007bf9f2
AmreetKhuntia <amreetleo123@gmail.com>
2025-07-14 18:00:59 +0530

    feat: added button mdx page
    -  added button mdx page

--- src/lib/components/code/props.ts
+++ src/lib/components/code/props.ts
@@ -5,17 +5,12 @@ export type CodeProps = {
   notCopiedIcon: string;
 };
 
-export const defaultCodeProps: CodeProps = {
-  copiedIcon: 'tick-icon.png',
-  notCopiedIcon: 'copy-icon.png',
-};
-
 export const defaultInTransition: TransitionWrapper = {
-  transition: { x: 40, duration: 400 },
+  transition: { x: 400, duration: 400 },
   transitionType: 'fly-and-fade',
 };
 
 export const defaultOutTransition: TransitionWrapper = {
-  transition: { x: 40, duration: 400 },
+  transition: { x: 400, duration: 400 },
   transitionType: 'fly-and-fade',
 };
