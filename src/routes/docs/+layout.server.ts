f2ce13d8bee3b17565926f1fa889a8a3258e88cc
AmreetKhuntia <amreetleo123@gmail.com>
2025-07-19 04:32:04 +0530

    fix: fixed routing for layout.server.ts
    - fixed routing for layout.server.ts

--- src/routes/docs/+layout.server.ts
+++ src/routes/docs/+layout.server.ts
@@ -1,38 +1,24 @@
-import fs from 'fs';
-import path from 'path';
 import type { SideNavItem } from '$lib/components/sideNav/props';
 
-const docsPath = path.resolve(process.cwd(), 'src/lib/docs');
-
-function getNavItems(dir: string, basePath: string = ''): SideNavItem[] {
-  const items: SideNavItem[] = [];
-  const files = fs.readdirSync(dir);
-
-  for (const file of files) {
-    const fullPath = path.join(dir, file);
-    const stat = fs.statSync(fullPath);
-    const href = path.join('/docs', basePath, file.replace('.mdx', ''));
-
-    if (stat.isDirectory()) {
-      items.push({
-        label: file,
-        href: '',
-        expanded: true,
-        children: getNavItems(fullPath, path.join(basePath, file)),
-      });
-    } else if (file.endsWith('.mdx')) {
-      items.push({
-        label: file.replace('.mdx', ''),
-        href,
-      });
-    }
-  }
-
-  return items;
-}
-
 export function load() {
-  const items = getNavItems(docsPath);
+  const items: SideNavItem[] = [
+    {
+      label: 'introduction',
+      href: '/docs/introduction',
+    },
+    {
+      label: 'components',
+      href: '',
+      expanded: true,
+      children: [
+        {
+          label: 'button',
+          href: '/docs/components/button',
+        },
+      ],
+    },
+  ];
+
   return {
     items,
   };
