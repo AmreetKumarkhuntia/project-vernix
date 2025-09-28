14623b9ddac1ebd924bcdd2aed472db0272013a9
AmreetKhuntia <amreetleo123@gmail.com>
2025-09-28 16:24:10 +0530

    feat: add documentation for all components
    - Create `.mdx` documentation files for all components in `src/lib/components`.
    - Update `src/routes/docs/[...slug]/+page.svelte` to dynamically load and render the documentation.
    - Update `src/routes/docs/+layout.server.ts` to add links to all the new documentation pages in the side navigation.
    - Populate the documentation files with content and examples based on the component's source code and props.
    - Update all component documentation to match the format of `button.mdx`.
    - Move "Installation", "Accessibility", "Troubleshooting", and "Best Practices" sections to the `introduction.mdx` file.
    - Update component imports in all `.mdx` files to use `$lib` instead of `vergins`.

--- src/routes/docs/+layout.server.ts
+++ src/routes/docs/+layout.server.ts
@@ -12,16 +12,104 @@ export function load() {
       expanded: true,
       children: [
         {
+          label: 'accordion',
+          href: '/docs/components/accordion',
+        },
+        {
+          label: 'avatar',
+          href: '/docs/components/avatar',
+        },
+        {
+          label: 'breadcrumbs',
+          href: '/docs/components/breadcrumbs',
+        },
+        {
           label: 'button',
           href: '/docs/components/button',
+        },
+        {
+          label: 'card',
+          href: '/docs/components/card',
+        },
+        {
+          label: 'carousel',
+          href: '/docs/components/carousel',
+        },
+        {
+          label: 'checkbox',
+          href: '/docs/components/checkbox',
+        },
+        {
+          label: 'code',
+          href: '/docs/components/code',
+        },
+        {
+          label: 'date',
+          href: '/docs/components/date',
+        },
+        {
+          label: 'drop-down',
+          href: '/docs/components/drop-down',
+        },
+        {
+          label: 'file-uploader',
+          href: '/docs/components/file-uploader',
+        },
+        {
+          label: 'flyer',
+          href: '/docs/components/flyer',
         },
         {
           label: 'header',
           href: '/docs/components/header',
         },
         {
-          label: 'inputField',
+          label: 'input-field',
           href: '/docs/components/input-field',
+        },
+        {
+          label: 'lazy-loader',
+          href: '/docs/components/lazy-loader',
+        },
+        {
+          label: 'modal',
+          href: '/docs/components/modal',
+        },
+        {
+          label: 'navbar',
+          href: '/docs/components/navbar',
+        },
+        {
+          label: 'progress-bar',
+          href: '/docs/components/progress-bar',
+        },
+        {
+          label: 'radio-group',
+          href: '/docs/components/radio-group',
+        },
+        {
+          label: 'ripple',
+          href: '/docs/components/ripple',
+        },
+        {
+          label: 'side-nav',
+          href: '/docs/components/side-nav',
+        },
+        {
+          label: 'time-line',
+          href: '/docs/components/time-line',
+        },
+        {
+          label: 'toggle',
+          href: '/docs/components/toggle',
+        },
+        {
+          label: 'tooltip',
+          href: '/docs/components/tooltip',
+        },
+        {
+          label: 'wrapper-text',
+          href: '/docs/components/wrapper-text',
         },
       ],
     },
--- src/routes/docs/[...slug]/+page.svelte
+++ src/routes/docs/[...slug]/+page.svelte
@@ -1,23 +1,89 @@
 <script lang="ts">
-  import Introduction   from '$lib/docs/introduction.mdx';
-  import ButtonDocs     from '$lib/docs/components/button.mdx';
-  import Header         from '$lib/docs/components/header.mdx';
-  import InputField     from '$lib/docs/components/input-field.mdx';
+  import Introduction from '$lib/docs/introduction.mdx';
+  import Accordion from '$lib/docs/components/accordion.mdx';
+  import Avatar from '$lib/docs/components/avatar.mdx';
+  import Breadcrumbs from '$lib/docs/components/breadcrumbs.mdx';
+  import Button from '$lib/docs/components/button.mdx';
+  import Card from '$lib/docs/components/card.mdx';
+  import Carousel from '$lib/docs/components/carousel.mdx';
+  import Checkbox from '$lib/docs/components/checkbox.mdx';
+  import Code from '$lib/docs/components/code.mdx';
+  import Date from '$lib/docs/components/date.mdx';
+  import DropDown from '$lib/docs/components/drop-down.mdx';
+  import FileUploader from '$lib/docs/components/file-uploader.mdx';
+  import Flyer from '$lib/docs/components/flyer.mdx';
+  import Header from '$lib/docs/components/header.mdx';
+  import InputField from '$lib/docs/components/input-field.mdx';
+  import LazyLoader from '$lib/docs/components/lazy-loader.mdx';
+  import Modal from '$lib/docs/components/modal.mdx';
+  import Navbar from '$lib/docs/components/navbar.mdx';
+  import ProgressBar from '$lib/docs/components/progress-bar.mdx';
+  import RadioGroup from '$lib/docs/components/radio-group.mdx';
+  import Ripple from '$lib/docs/components/ripple.mdx';
+  import SideNav from '$lib/docs/components/side-nav.mdx';
+  import TimeLine from '$lib/docs/components/time-line.mdx';
+  import Toggle from '$lib/docs/components/toggle.mdx';
+  import Tooltip from '$lib/docs/components/tooltip.mdx';
+  import WrapperText from '$lib/docs/components/wrapper-text.mdx';
 
   import { page } from '$app/stores';
 
-  $: slug = $page.params.slug; // type: string[]
+  $: slug = $page.params.slug;
 </script>
 
 <div class="prose">
-  {#if slug === 'components/button'}
-    <ButtonDocs />
-  {:else if slug === 'introduction'}
-    <Introduction />
+  {#if slug === 'components/accordion'}
+    <Accordion />
+  {:else if slug === 'components/avatar'}
+    <Avatar />
+  {:else if slug === 'components/breadcrumbs'}
+    <Breadcrumbs />
+  {:else if slug === 'components/button'}
+    <Button />
+  {:else if slug === 'components/card'}
+    <Card />
+  {:else if slug === 'components/carousel'}
+    <Carousel />
+  {:else if slug === 'components/checkbox'}
+    <Checkbox />
+  {:else if slug === 'components/code'}
+    <Code />
+  {:else if slug === 'components/date'}
+    <Date />
+  {:else if slug === 'components/drop-down'}
+    <DropDown />
+  {:else if slug === 'components/file-uploader'}
+    <FileUploader />
+  {:else if slug === 'components/flyer'}
+    <Flyer />
   {:else if slug === 'components/header'}
     <Header />
   {:else if slug === 'components/input-field'}
     <InputField />
+  {:else if slug === 'components/lazy-loader'}
+    <LazyLoader />
+  {:else if slug === 'components/modal'}
+    <Modal />
+  {:else if slug === 'components/navbar'}
+    <Navbar />
+  {:else if slug === 'components/progress-bar'}
+    <ProgressBar />
+  {:else if slug === 'components/radio-group'}
+    <RadioGroup />
+  {:else if slug === 'components/ripple'}
+    <Ripple />
+  {:else if slug === 'components/side-nav'}
+    <SideNav />
+  {:else if slug === 'components/time-line'}
+    <TimeLine />
+  {:else if slug === 'components/toggle'}
+    <Toggle />
+  {:else if slug === 'components/tooltip'}
+    <Tooltip />
+  {:else if slug === 'components/wrapper-text'}
+    <WrapperText />
+  {:else if slug === 'introduction'}
+    <Introduction />
   {:else}
     <div>documentation is not available yet.</div>
   {/if}
